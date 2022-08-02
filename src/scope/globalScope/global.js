2e5781eca66d4f82882dfbba5287f52dbdd63bd9
totemiclynx <marlon9945@gmail.com>
2022-08-02 14:46:06 -0500

    Clase #2 Global Scope

---
+++ src/scope/globalScope/global.js
@@ -0,0 +1,26 @@
+//Variables
+
+/* var a; //Declarando una variable
+var b = 'b'; //Declarando y asignando inmediatamente una variable
+b = 'bb'; //Reasignando un valor
+var a = 'aa'; //Redelcarando una variable
+
+//Global Scope
+
+var fruit = 'Apple'; //Global
+console.log(fruit);
+
+function bestFruit(){
+    console.log(fruit);
+}
+
+bestFruit(); */
+
+
+function countries(){
+    country = 'Colombia';
+    console.log(country);
+}
+
+countries();
+console.log(country);
