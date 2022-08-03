2e38260382cbac750b7d83647a0fbe9174fecc44
totemiclynx <marlon9945@gmail.com>
2022-08-03 15:15:43 -0500

    Se agrega clase hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,11 @@
+/* console.log(nameOfDOg);
+var nameOfDOg = "Jacob";
+console.log(nameOfDOg); */
+
+nameOfDOg();
+
+function nameOfDOg(){
+    console.log("El mejor perrito es: " + elmo);
+}
+
+var elmo = 'Benjamin';
